9fcc8b19a5d2d1245737d437181a1e858e072565
ndavispe <157775257+notnatedavis@users.noreply.github.com>
2025-03-24 18:43:21 +0000

    Assignment1 : 2.3

---
+++ Assignment1/JavaGradle/src/main/java/Fraction.java
@@ -0,0 +1,71 @@
+import java.io.*;
+/**
+ * Purpose: demonstrate simple Java Fraction class with command line,
+ * jdb debugging, and Ant build file.
+ *
+ * Ser321 Foundations of Distributed Applications
+ * see http://pooh.poly.asu.edu/Ser321
+ * @author Tim Lindquist Tim.Lindquist@asu.edu
+ *         Software Engineering, CIDSE, IAFSE, ASU Poly
+ * @version January 2020
+ */
+public class Fraction {
+
+   private int numerator, denominator;
+
+   public Fraction(){
+      numerator = denominator = 0;
+   }
+
+   public void print() {
+    System.out.print(numerator + "/" + denominator );
+   }
+
+   public void setNumerator (int n ){
+      numerator = n;
+   }
+
+   public void setDenominator (int d) {
+      denominator = d;
+   }
+
+   public int getDenominator() {
+      return denominator;
+   }
+
+   public int getNumerator() {
+      return numerator;
+   }
+
+   public static void main (String args[]) {
+      try {
+         // create a new instance
+         // Fraction *frac = [[Fraction alloc] init];
+         Fraction frac = new Fraction();
+
+         // Assignment 1 : 2.3
+         int num = 1; // default numerator
+         int denom = 3; // default denominator
+         // parse command line args
+         if (args.length >= 1) {
+            num = Integer.parseInt(args[0]);
+         }
+         if (args.length >= 2) {
+            denom = Integer.parseInt(args[1]);
+         }
+
+         // set the values
+         frac.setNumerator(1);
+         frac.setDenominator(3);
+
+         // print it
+         System.out.print("The fraction is: ");
+         frac.print();
+         System.out.println("");
+
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
+
